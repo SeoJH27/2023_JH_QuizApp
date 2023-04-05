bc8d33667d90b4be7f4c9579dcef9c586e36b40b
SeoJH27 <morninglibrary@gmail.com>
2023-04-05 23:11:07 +0900

    중간 커밋 - 틀 잡는 중

--- app/src/main/java/com/example/jh_quizapp/MainActivity.java
+++ app/src/main/java/com/example/jh_quizapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.jh_quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    View v1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        v1 = findViewById(R.id.layout);
     }
+
+    public void setFrag1(View v){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        ft.replace(R.id.frame_container, new Fragment1(), "one");
+        ft.commitAllowingStateLoss();
+    }
+    public void setFrag2(View v){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        ft.replace(R.id.frame_container, new Fragment2(), "two");
+        ft.commitAllowingStateLoss();
+    }
+
+
+
 }
